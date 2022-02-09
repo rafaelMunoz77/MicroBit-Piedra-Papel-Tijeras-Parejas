8dcb851ba1e02bd9984b53c999c5b0927b627c0f
rafaelMunoz77 <32701274+rafaelMunoz77@users.noreply.github.com>
2022-02-09 08:49:49 +0100

    guardo

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-
+input.onGesture(Gesture.Shake, function () {
+    MiJugada = randint(1, 3)
+    if (MiJugada == 1) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else if (MiJugada == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . .
+            # # # . #
+            . . . # .
+            # # # . #
+            # # . . .
+            `)
+    }
+})
+let MiJugada = 0
+radio.setGroup(1)
