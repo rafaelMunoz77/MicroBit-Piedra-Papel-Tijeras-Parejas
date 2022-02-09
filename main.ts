db47b8ac2b6802d78505a2ecc747b45de4d7aeee
rafaelMunoz77 <32701274+rafaelMunoz77@users.noreply.github.com>
2022-02-09 09:08:43 +0100

    Guardar

--- main.ts
+++ main.ts
@@ -1,5 +1,34 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Jugada2 = randint(1, 3)
+    if (Jugada2 == 1) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else if (Jugada2 == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     MiJugada = randint(1, 3)
+    radio.sendNumber(MiJugada)
     if (MiJugada == 1) {
         basic.showLeds(`
             . # # # .
@@ -18,13 +47,14 @@ input.onGesture(Gesture.Shake, function () {
             `)
     } else {
         basic.showLeds(`
-            # # . . .
-            # # # . #
-            . . . # .
-            # # # . #
-            # # . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
             `)
     }
 })
 let MiJugada = 0
+let Jugada2 = 0
 radio.setGroup(1)
